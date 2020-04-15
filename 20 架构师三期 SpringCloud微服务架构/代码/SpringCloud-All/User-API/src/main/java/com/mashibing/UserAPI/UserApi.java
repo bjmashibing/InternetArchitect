d631a68e1d62aa323d955f6202486260aad42711
bjmashibing <alanpppbox@gmail.com>
2020-04-15 22:13:27 +0800

    1
    1

--- 20 架构师三期 SpringCloud微服务架构/代码/SpringCloud-All/User-API/src/main/java/com/mashibing/UserAPI/UserApi.java
+++ 20 架构师三期 SpringCloud微服务架构/代码/SpringCloud-All/User-API/src/main/java/com/mashibing/UserAPI/UserApi.java
@@ -1,12 +1,16 @@
 package com.mashibing.UserAPI;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/User")
 public interface UserApi {
 
 	/**
+	 * 老师，工作中有专门起一个公共api服务的吗
+	 * 
 	 * 查看当前服务状态~~~
 	 * @return (* ￣3)(ε￣ *)
 	 */
@@ -15,4 +19,8 @@ public interface UserApi {
 	
 	@GetMapping("/getById")
 	public String getById(Integer id);
+	
+	
+	@PostMapping("/postPserson")
+	public Person postPserson(@RequestBody Person person);
 }
--- 20 架构师三期 SpringCloud微服务架构/代码/SpringCloud-All/User-Provider/src/main/java/com/mashibing/UserProvider/UserController.java
+++ 20 架构师三期 SpringCloud微服务架构/代码/SpringCloud-All/User-Provider/src/main/java/com/mashibing/UserProvider/UserController.java
@@ -1,18 +1,45 @@
 package com.mashibing.UserProvider;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mashibing.UserAPI.Person;
 import com.mashibing.UserAPI.UserApi;
 
 @RestController
 public class UserController implements UserApi {
 
 	
+	@Value("${server.port}")
+	String port;
+	
+	
+	private AtomicInteger count = new AtomicInteger();
+	
 	@Override
 	public String alive() {
 
+		try {
+			System.out.println("准备睡");
+			
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-		return "ooxxoo";
+		int i = count.getAndIncrement();
+		System.out.println(port + " 好的 ====第：" + i + "次调用");
+		return "port:" + port;
 	}
 
 	@Override
@@ -20,18 +47,41 @@ public class UserController implements UserApi {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-	/**
-	 * 写一个文档  给谁看？ php看吗
-	 * 
-	 * 接口写了，只有java的客户端能用，php 再去写文档。。。
-	 * 
-	 * 
-	 * 
-	 * 服务名
-	 * 接口名。。。。
-	 * @return
-	 */
 	
+	@GetMapping("/getMap")
+	public Map<Integer, String> getMap(@RequestParam("id") Integer id) {
+		
+		
+		
+		System.out.println(id);
+		return Collections.singletonMap(id, "mmeme");
+	}
+	@GetMapping("/getMap2")
+	public Map<Integer, String> getMap2(Integer id,String name) {
+		// TODO Auto-generated method stub
+		System.out.println(id);
+		return Collections.singletonMap(id, name);
+	}
+	
+	@GetMapping("/getMap3")
+	public Map<Integer, String> getMap3(@RequestParam Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		System.out.println(map);
+		return Collections.singletonMap(Integer.parseInt(map.get("id").toString()), map.get("name").toString());
+	}
+	
+	
+	@PostMapping("/postMap")
+	public Map<Integer, String> postMap(@RequestBody Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		System.out.println(map);
+		return Collections.singletonMap(Integer.parseInt(map.get("id").toString()), map.get("name").toString());
+	}
+
+	@Override
+	public Person postPserson(Person person) {
+		System.out.println(ToStringBuilder.reflectionToString(person));
+		return person;
+	}
 
 }
